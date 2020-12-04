b7bc183064c2b9620d2022c83fb1bc87e9b3cff8
rsoylu <70935031+rsoylu@users.noreply.github.com>
2020-12-03 23:49:36 -0600

    Add slider, adjust map page

--- js/script.js
+++ js/script.js
@@ -1,71 +1,77 @@
   let Popup, popup;
   
   function initMap(){
-  var map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 41.048750, lng: 28.792600},
-      zoom: 15
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 41.048750, lng: 28.792600},
+        zoom: 15
+      });
+      
+      map.setMapTypeId('satellite');
+      
+    const marker = new google.maps.Marker({
+      position: {lat: 41.048750, lng: 28.792600},
+      map: map,
+      animation: google.maps.Animation.DROP,
     });
     
-    map.setMapTypeId('satellite');
+    class Popup extends google.maps.OverlayView {
+      constructor(position, content) {
+        super();
+        this.position = position;
+        content.classList.add("popup-bubble");
+        // This zero-height div is positioned at the bottom of the bubble.
+        const bubbleAnchor = document.createElement("div");
+        bubbleAnchor.classList.add("popup-bubble-anchor");
+        bubbleAnchor.appendChild(content);
+        // This zero-height div is positioned at the bottom of the tip.
+        this.containerDiv = document.createElement("div");
+        this.containerDiv.classList.add("popup-container");
+        this.containerDiv.appendChild(bubbleAnchor);
+        // Optionally stop clicks, etc., from bubbling up to the map.
+        Popup.preventMapHitsAndGesturesFrom(this.containerDiv);
+      }
+      /** Called when the popup is added to the map. */
+      onAdd() {
+        this.getPanes().floatPane.appendChild(this.containerDiv);
+      }
+      /** Called when the popup is removed from the map. */
+      onRemove() {
+        if (this.containerDiv.parentElement) {
+          this.containerDiv.parentElement.removeChild(this.containerDiv);
+        }
+      }
+      /** Called each frame when the popup needs to draw itself. */
+      draw() {
+        const divPosition = this.getProjection().fromLatLngToDivPixel(
+          this.position
+        );
+        // Hide the popup when it is far out of view.
+        const display =
+          Math.abs(divPosition.x) < 4000 && Math.abs(divPosition.y) < 4000
+            ? "block"
+            : "none";
+
+        if (display === "block") {
+          this.containerDiv.style.left = divPosition.x + "px";
+          this.containerDiv.style.top = divPosition.y + "px";
+        }
+
+        if (this.containerDiv.style.display !== display) {
+          this.containerDiv.style.display = display;
+        }
+      }
+    }
+    popup = new Popup(
+      new google.maps.LatLng(41.05,28.792600),
+      document.getElementById("content")
+    );
+    popup.setMap(map);
     
-  const marker = new google.maps.Marker({
-    position: {lat: 41.048750, lng: 28.792600},
-    map: map,
-    animation: google.maps.Animation.DROP,
-  });
-  
-  class Popup extends google.maps.OverlayView {
-    constructor(position, content) {
-      super();
-      this.position = position;
-      content.classList.add("popup-bubble");
-      // This zero-height div is positioned at the bottom of the bubble.
-      const bubbleAnchor = document.createElement("div");
-      bubbleAnchor.classList.add("popup-bubble-anchor");
-      bubbleAnchor.appendChild(content);
-      // This zero-height div is positioned at the bottom of the tip.
-      this.containerDiv = document.createElement("div");
-      this.containerDiv.classList.add("popup-container");
-      this.containerDiv.appendChild(bubbleAnchor);
-      // Optionally stop clicks, etc., from bubbling up to the map.
-      Popup.preventMapHitsAndGesturesFrom(this.containerDiv);
-    }
-    /** Called when the popup is added to the map. */
-    onAdd() {
-      this.getPanes().floatPane.appendChild(this.containerDiv);
-    }
-    /** Called when the popup is removed from the map. */
-    onRemove() {
-      if (this.containerDiv.parentElement) {
-        this.containerDiv.parentElement.removeChild(this.containerDiv);
-      }
-    }
-    /** Called each frame when the popup needs to draw itself. */
-    draw() {
-      const divPosition = this.getProjection().fromLatLngToDivPixel(
-        this.position
-      );
-      // Hide the popup when it is far out of view.
-      const display =
-        Math.abs(divPosition.x) < 4000 && Math.abs(divPosition.y) < 4000
-          ? "block"
-          : "none";
-
-      if (display === "block") {
-        this.containerDiv.style.left = divPosition.x + "px";
-        this.containerDiv.style.top = divPosition.y + "px";
-      }
-
-      if (this.containerDiv.style.display !== display) {
-        this.containerDiv.style.display = display;
-      }
-    }
-  }
-  popup = new Popup(
-    new google.maps.LatLng(41.05,28.792600),
-    document.getElementById("content")
-  );
-  popup.setMap(map);
+    
+    new Splide( '.splide', {
+		'cover'      : true,
+		'heightRatio': 0.7,
+	} ).mount();
   
   }
   
