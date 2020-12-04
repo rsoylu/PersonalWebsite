60b793fbb60ff75949baddc9c2eb60f85d7b0e1d
rsoylu <70935031+rsoylu@users.noreply.github.com>
2020-12-03 22:36:18 -0600

    update map

--- js/script.js
+++ js/script.js
@@ -4,8 +4,6 @@
   var map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 41.048750, lng: 28.792600},
       zoom: 15
-      
-
     });
     
     map.setMapTypeId('satellite');
